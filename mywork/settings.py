9fc73f13f0b9fca5046ba4e223031cb2d1cb0c72
AlankarSaxena2712 <saxenaalankar42@gmail.com>
2021-01-21 13:01:20 +0530

    email setting changed

--- mywork/settings.py
+++ mywork/settings.py
@@ -138,9 +138,9 @@ LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get("USER_EMAIL")
 EMAIL_HOST_PASSWORD = os.environ.get("USER_EMAIL_PASS")
 
